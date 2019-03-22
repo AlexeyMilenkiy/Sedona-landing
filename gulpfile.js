50b19630f5c3db15372a098f15849bfc01d20497
Alex Milenkiy <alex.milenkiy@gmail.com>
2019-03-22 22:11:42 +0300

    start using less

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,28 @@ var gulp = require('gulp');
 
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', function(){
+var less = require('gulp-less');
+
+gulp.task('less', function(){
+
+	return gulp.src('./less/main.less')
+	       .pipe(less())
+	       .pipe(gulp.dest('./style'))
+	       .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('less', function(){
        
        browserSync.init({
 
        	server: {baseDir: './'}
        })
 
-
 	gulp.watch('./index.html').on('change', browserSync.reload);
 
     gulp.watch('./style/*.css').on('change', browserSync.reload);
+    
+    gulp.watch('./less/*.less', gulp.series('less'));
 
-    gulp.watch('./js/*.js').on('change', browserSync.reload);
-
-});
+}));
 
