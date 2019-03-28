e3811ab490cee2491f3b125490ba996a97bf6419
Alex Milenkiy <alex.milenkiy@gmail.com>
2019-03-28 21:43:32 +0300

    add favicon

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,12 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var concat = require('gulp-concat');
 var order = require('gulp-order');
+var realFavicon = require ('gulp-real-favicon');
+var fs = require('fs');
 
 gulp.task('less', function(){
 
-	return gulp.src(['./less/variables.less', 
-                   './less/mixines.less', 
-                   './less/header.less', 
-                   './less/main-first-block.less',
-                   './less/main-second-block.less',  
-                   './less/footer.less'])
+	return gulp.src(['./less/*.less'])
 	       .pipe(less())
          .pipe(concat('main.css'))
 	       .pipe(gulp.dest('./style/dest'))
@@ -44,3 +41,63 @@ gulp.task('default', gulp.series('less', 'css', function(){
 
 }));
 
+var FAVICON_DATA_FILE = 'faviconData.json';
+
+gulp.task('generate-favicon', function(done) {
+  realFavicon.generateFavicon({
+    masterPicture: './img/logotype-tablet.png',
+    dest: './img/icons',
+    iconsPath: '/',
+    design: {
+      ios: {
+        pictureAspect: 'backgroundAndMargin',
+        backgroundColor: '#ffffff',
+        margin: '21%'
+      },
+      desktopBrowser: {},
+      windows: {
+        pictureAspect: 'whiteSilhouette',
+        backgroundColor: '#da532c',
+        onConflict: 'override'
+      },
+      androidChrome: {
+        pictureAspect: 'shadow',
+        themeColor: '#ffffff',
+        manifest: {
+          name: 'PUGOFKA',
+          display: 'browser',
+          orientation: 'notSet',
+          onConflict: 'override'
+        }
+      },
+      safariPinnedTab: {
+        pictureAspect: 'silhouette',
+        themeColor: '#5bbad5'
+      }
+    },
+    settings: {
+      compression: 5,
+      scalingAlgorithm: 'Mitchell',
+      errorOnImageTooSmall: false
+    },
+    markupFile: FAVICON_DATA_FILE
+  }, function() {
+    done();
+  });
+});
+
+gulp.task('inject-favicon-markups', function() {
+  gulp.src([ './*.html' ])
+    .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+    .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('check-for-favicon-update', function(done) {
+  var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+  realFavicon.checkForUpdates(currentVersion, function(err) {
+    if (err) {
+      throw err;
+    }
+  });
+});
