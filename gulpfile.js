5115beb7279bc8958bf941e53c255e576a2528b4
Alex Milenkiy <alex.milenkiy@gmail.com>
2019-03-28 15:19:24 +0300

    separate less files

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,34 @@
 var gulp = require('gulp');
-
 var browserSync = require('browser-sync').create();
-
 var less = require('gulp-less');
-
 var postcss = require('gulp-postcss');
-
 var autoprefixer = require('autoprefixer');
+var concat = require('gulp-concat');
+var order = require('gulp-order');
 
 gulp.task('less', function(){
 
-	return gulp.src('./less/main.less')
-	       .pipe(less({includePaths: require('node-normalize-scss').includePaths}))
+	return gulp.src(['./less/variables.less', 
+                   './less/mixines.less', 
+                   './less/header.less', 
+                   './less/main-first-block.less',
+                   './less/main-second-block.less',  
+                   './less/footer.less'])
+	       .pipe(less())
+         .pipe(concat('main.css'))
 	       .pipe(gulp.dest('./style/dest'))
 	       .pipe(browserSync.stream());
 });
 
 gulp.task('css', function () {
  var processors = [autoprefixer];
-  return gulp.src('./style/dest/*.css')
+  return gulp.src('./style/dest/main.css')
          .pipe(postcss(processors))
-         .pipe(gulp.dest('./style'))
+         .pipe(gulp.dest('./style/'))
          .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('less', 'css', function(){
+gulp.task('default', gulp.series('less', 'css', function(){
        
        browserSync.init({
 
@@ -33,11 +37,10 @@ gulp.task('default', gulp.parallel('less', 'css', function(){
 
 	gulp.watch('./index.html').on('change', browserSync.reload);
 
-    gulp.watch('./style/*.css').on('change', browserSync.reload);
-
-    gulp.watch('./style/media/*.css').on('change', browserSync.reload);
-    
+    gulp.watch('./style/main.css').on('change', browserSync.reload);
+ 
     gulp.watch('./less/*.less', gulp.series('less'));
+    gulp.watch('./style/dest/*.css', gulp.series('css'));
 
 }));
 
