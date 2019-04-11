d0b7a219b40f6beb399e312fa74b3e6ffad7eba8
Distillery <distillery@Mac-mini-iOS.local>
2019-04-11 14:52:15 +0300

    work slider

--- js/slider.js
+++ js/slider.js
@@ -1,149 +1,128 @@
-window.onload = function(){
+window.onload = function () {
 
-   let slides = document.querySelectorAll('.slide-single');
-   let next = document.querySelector('#next-slide');
-   let previous = document.querySelector('#prev-slide');
-   let slider = [];
+    const slides = document.querySelectorAll('.slide-single');
+    const next = document.querySelector('#next-slide');
+    const previous = document.querySelector('#prev-slide');
+    const sliderBlock = document.querySelector('.slider')
+    let slider = [];
 
-   for (let i=0; i< slides.length; i++){
-   	slider[i] = slides[i].src;   // присвоение атрибута, добавляем путь к нашей картинке
-   	slides[i].remove();  // удаление картинок со страницы
-   }
-   console.log(slider); // остался только массив путей
+    for (let i = 0; i < slides.length; i++) {
+        slider[i] = slides[i].src;   // присвоение атрибута, добавляем путь к нашей картинке
+        slides[i].remove();  // удаление картинок со страницы
+    }
 
-   let step = 0; // определяет какую картинку показывать в основном блоке
-   let offset = 0; // смещение картинки
-   
-   // функция отрисовки изображения
+    // console.log(slider); // остался только массив путей
 
-   function showImage(){
-   	    let img = document.createElement('img'); // создание картинки
-   	    img.src = slider[step]; // присвоение пути каждой новой картинке 
-   	    img.classList.add('slide-single'); // добавляем картинке класс
-   	    img.style.left = offset * 806 + 'px'; // смещение картинки на ее ширину, первая картинка не смещена она по центру
-   	    document.querySelector('.slider').appendChild(img); // добавляем картинку в наш блок
-        if(step + 1 === slider.length){   
-        	step = 0;
+    let step = 0; // определяет какую картинку показывать в основном блоке
+    let offset = 0; // смещение картинки
+
+    // функция отрисовки изображений
+    function showImage() {
+        let step = 1;
+        let offset1 = 1;
+        let img = document.createElement('img'); // создание картинки
+        let img1 = document.createElement('img');
+        let img2 = document.createElement('img'); // создание картинки
+
+        img.src = slider[step - 1]; // присвоение пути каждой новой картинке
+        img.classList.add('slide-single'); // добавляем картинке класс
+        img.style.left = offset1 - 806 + 'px'; // смещение картинки на ее ширину, первая картинка не смещена она по центру
+        sliderBlock.appendChild(img); // добавляем картинку в наш блок
+
+        img1.src = slider[step]; // присвоение пути каждой новой картинке
+        img1.classList.add('slide-single'); // добавляем картинке класс
+        img1.style.left = offset * 806 + 'px'; // смещение картинки на ее ширину, первая картинка не смещена она по центру
+        sliderBlock.appendChild(img1);// добавляем картинку в наш блок
+
+        img2.src = slider[step + 1]; // присвоение пути каждой новой картинке
+        img2.classList.add('slide-single'); // добавляем картинке класс
+        img2.style.left = offset1 * 806 + 'px'; // смещение картинки на ее ширину, первая картинка не смещена она по центру
+        sliderBlock.appendChild(img2);// добавляем картинку в наш блок
+    }
+
+    function addNextImage() {
+        let rightImg = document.createElement('img');
+
+        console.log("номер картинки после нажатия кнопки" + step);
+        if (step === 5) {
+            step = 0;
+            console.log("первое условие" + step);
+        } else {
+            step += 1;
+            console.log("второе условие" + step);
         }
-        else{
-        	step++;
+        rightImg.src = slider[step]; // присвоение пути каждой новой картинке
+        rightImg.classList.add('slide-single'); // добавляем картинке класс
+        rightImg.style.left = 806 + 'px'; // смещение картинки на ее ширину, первая картинка не смещена она по центру
+        sliderBlock.appendChild(rightImg);// добавляем картинку в наш блок
+    }
+
+    function addPrevImage() {
+        let leftImg = document.createElement('img');
+        if (step === -1) {
+            step = slides.length - 1;       // присвоение пути каждой новой картинке
+        } else {
+            leftImg.src = slider[step - 1]; // присвоение пути каждой новой картинке
         }
-        offset = 1;
-   }
+        leftImg.src = slider[step];
+        leftImg.classList.add('slide-single'); // добавляем картинке класс
+        leftImg.style.left = -806 + 'px'; // смещение картинки на ее ширину, первая картинка не смещена она по центру
+        sliderBlock.insertBefore(leftImg, sliderBlock.children[0]);// добавляем картинку в наш блок
+    }
 
-   function left(){
-   	    document.onclick = null;
-   	    let slides2 = document.querySelectorAll('.slide-single');
-   	    let offset2 = 0;
+    function nextImage() {
 
-   	    for(let i=0; i<slides2.length; i++){
-   	    	slides2[i].style.left = offset2 * 806 - 806 + 'px';
-   	    	offset2++;
-   	    }
-   	       setTimeout(function(){
-   	           
-   	           showImage();
-   	           slides2[0].remove();
-   	           document.onclick = left;
-   	       }, 3000);
-   }
+        let offset2 = 0;
+        let step2 = 0;
+        let slides2 = document.querySelectorAll('.slide-single');
 
-      function right(){
-   	    document.onclick = null;
-   	    let slides2 = document.querySelectorAll('.slide-single');
-   	    let offset2 = 0;
+        slides2[0].remove();
 
-   	    for(let i=0; i<slides2.length; i++){
-   	    	slides2[i].style.right = offset2 * 806 + 806 + 'px';
-   	    	offset2++;
-   	    }
-   	       setTimeout(function(){
-   	           slides2[0].remove();
-   	           showImage();
-   	           document.onclick = right;
-   	       }, 3000);
-   }
-   next.onclick = left;
-   previous.onclick  = right;
-   
-   showImage();showImage();
-}
+        for (let i = 0; i < slides2.length; i++) {
 
-// $(document).ready(function(){
-
-// 	Slider(20);
+            if (step2 + 1 === slides2.length) {
+                offset2 = 1;
 
 
-// 	function Slider(margin) {
+            } else {
+                offset2 = 0;
+                step2++;
+                step++;
+            }
+            slides2[i].style.left = offset2 * 806 - 806 + 'px';
+        }
+        setTimeout(function () {
+            addNextImage();
+        }, 3000);
+    }
 
-// 		// html var
-// 		var sliderContainer = $('#slider');
-// 		var sliderInner = $(sliderContainer).find('.slider-inner');
-// 		var item = $(sliderInner).find('.item');
-// 		var itemShow = 4; // Сколько показывать 4
-// 		var pagination = $(sliderContainer).find('.pagination');
+    function prevImage() {
 
-// 		// Head var
-// 		var itemL = item.length; // Количество  12
-// 		var sliderInnerWidth = sliderInner.width(); // 1170
-// 		var sliderWrapper = $(sliderInner).find('.slider-wrapper'); 
+        let offset3 = 0;
+        let step3 = 3;
+        let slides3 = document.querySelectorAll('.slide-single');
+        slides3[2].remove();
 
+        for (let i = 2; i >= 0; i--) {
 
-// 		// Item Width 
-// 		var itemW = (sliderInnerWidth - ((margin * 2) * itemShow)  ) / itemShow; //252
+            if (step3 - 1 > 0) {
+                offset3 = 1;
+                step3--;
+            } else {
+                offset3 = 0;
+                step--;
+            }
+            slides3[i].style.left = offset3 * 806 + 'px';
+        }
 
+        setTimeout(function () {
+            addPrevImage();
+        }, 3000);
+    }
 
-// 		// Wrapper width
-// 		var sliderWrapperWidth =  itemL * ( itemW + (margin * 2)); // 3510
+    next.onclick = nextImage;
+    previous.onclick = prevImage;
 
-// 		// Максимальное значение шага
-// 		var nextMax = sliderWrapperWidth - sliderInnerWidth;
-// 		nextMax = nextMax * (-1);
-// 		var td = 0;
-// 		var step;
+    showImage();
 
-// 		$('.prev').click(function(){
-
-// 			if (td == 0) {
-// 				return false;
-// 			}
-// 			else {
-// 				step = itemW + (margin * 2);
-// 				td += step
-// 				$('.slider-wrapper').css({
-// 					'transform' : 'translate3d(' + td + 'px, 0px, 0px)'
-// 				});
-// 				console.log(td);
-// 			}
-// 		});
-// 		$('.next').click(function(){
-			
-// 			if(nextMax == td) {
-// 				return false
-// 			}
-// 			else {
-// 				step = itemW + (margin * 2);
-
-// 				td -= step
-// 				$('.slider-wrapper').css({
-// 					'transform' : 'translate3d(' + td + 'px, 0px, 0px)'
-// 				});
-// 				console.log(td);
-// 			}
-// 		});
-
-// 		$(sliderWrapper).css({
-// 			'width': sliderWrapperWidth + 'px'
-// 		});
-
-// 		$(item).css({
-// 			'width' : itemW + 'px',
-// 			'margin' : margin + 'px'
-// 		});
-
-
-// 	};
-
-// });
-
-
+};
