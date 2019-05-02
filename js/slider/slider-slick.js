5e352b5138c70567daef3d06074838d2e85c5fda
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2019-05-02 23:36:47 +0300

    change pagination

--- js/slider/slider-slick.js
+++ js/slider/slider-slick.js
@@ -1,34 +1,63 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.slider').slick({
+
         dots: true,
 
-        customPaging : function(slider, i) {
+        customPaging: function (slider, i) {
             var thumb = $(slider.$slides[i]).data();
-            return '<button class="img-navigation">' + (i+1) + '</button>';
+            let indexPaging = 3;
+
+            console.log($('li'));
+            return '<button class="img-navigation">' + (i + 1) + '</button>';
         },
 
         arrows: true,
 
         appendArrows: '.pagination-block',
 
-        prevArrow : '<button class="slide-navigation">Назад</button>',
+        prevArrow: '<button class="slide-navigation">Назад</button>',
 
-        appendDots : '.pagination-dots',
+        appendDots: '.pagination-dots',
 
-        nextArrow : '<button class="slide-navigation">Вперед</button>',
+        nextArrow: '<button class="slide-navigation">Вперед</button>',
 
         autoplay: false,
 
         speed: 2000,
 
-        dotsClass: "pagination-img-block"
+        dotsClass: 'pagination-dots',
+
+        // asNavFor: '.pagination-dots',
 
     });
 
-    // $('.pagination-img-block').slick({ // настройка навигации
-    //     slidesToShow: 3, // указываем что нужно показывать 3 навигационных изображения
+    // $('.img-navigation').slick({ // настройка навигации
+    //     // slidesToShow: 3, // указываем что нужно показывать 3 навигационных изображения
     //     asNavFor: '.slider', // указываем что это навигация для блока выше
     //     focusOnSelect: true // указываем что бы слайделось по клику
     // });
+
+    // $(".pagination-dots").slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: ".slider",
+    //     // nextArrow: ".slider__next",
+    //     // prevArrow: ".slider__prev",
+    //     focusOnSelect: true
+    // });
+
+    function addClass(){
+        $('li').find('.img-navigation').removeClass('active');
+        $('.slick-active').find('.img-navigation').addClass('active');
+    }
+    addClass();
+
+    $('li').on("click", function () {
+        addClass();
+    });
+
+    $('.slide-navigation').on('click', function(){
+        addClass();
+    });
 
 });
