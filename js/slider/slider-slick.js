d0e82eb35aa3ba5fbc5c44c95fdc1fca3c31d7fa
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2019-05-06 11:19:51 +0300

    change pagination

--- js/slider/slider-slick.js
+++ js/slider/slider-slick.js
@@ -1,63 +1,88 @@
-$(document).ready(function() {
-    $('.slider').slick({
+$(document).ready(() => {
+  const images = Array.from($('.slider img'));
 
-        dots: true,
+  images.forEach((item, i) => {
+    const newDots = document.createElement('button');
+    newDots.classList.add('img-navigation');
+    newDots.innerText = i + 1;
+    document.querySelector('.pagination-dots').appendChild(newDots);
+  });
 
-        customPaging: function (slider, i) {
-            var thumb = $(slider.$slides[i]).data();
-            let indexPaging = 3;
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    appendArrows: '.pagination-block',
+    prevArrow: '<button class="slide-navigation">Назад</button>',
+    nextArrow: '<button class="slide-navigation">Вперед</button>',
+    autoplay: false,
+    speed: 2000,
+  });
 
-            console.log($('li'));
-            return '<button class="img-navigation">' + (i + 1) + '</button>';
-        },
+  const indexPaging = 5;
+  $('.pagination-dots').slick({
+    slidesToShow: indexPaging,
+    slidesToScroll: indexPaging,
+    dots: false,
+    infinite: false,
+    focusOnSelect: false,
+    variableWidth: true,
+    prevArrow: '<button class="lessImg-class"><<</button>',
+    nextArrow: '<button class="moreImg-class">>></button>',
+  });
 
-        arrows: true,
+  $('.pagination-dots').on('click', '.slick-slide', function (event) {
+    event.preventDefault();
+    const goToSingleSlide = $(this).data('slick-index');
 
-        appendArrows: '.pagination-block',
+    $('.slider').slick('slickGoTo', goToSingleSlide);
+  });
 
-        prevArrow: '<button class="slide-navigation">Назад</button>',
+  $('.slider').on('afterChange', (event, slick, currentSlide) => {
+    $('.pagination-dots').slick('slickGoTo', currentSlide);
+    const currrentNavSlideElem = `.pagination-dots .slick-slide[data-slick-index="${currentSlide}"]`;
 
-        appendDots: '.pagination-dots',
+    console.log(currrentNavSlideElem);
 
-        nextArrow: '<button class="slide-navigation">Вперед</button>',
+    $('.pagination-dots .slick-slide.is-active').removeClass('is-active');
+    $(currrentNavSlideElem).addClass('active');
+  });
+  // $('.img-navigation').slick({ // настройка навигации
+  //     // slidesToShow: 3, // указываем что нужно показывать 3 навигационных изображения
+  //     asNavFor: '.slider', // указываем что это навигация для блока выше
+  //     focusOnSelect: true // указываем что бы слайделось по клику
+  // });
+  //
+  // $(".pagination-img-block").slick({
+  //     slidesToShow: 3,
+  //     slidesToScroll: 3,
+  //     asNavFor: ".slider",
+  //     arrows: false,
+  //     // nextArrow: ".slider__next",
+  //     // prevArrow: ".slider__prev",
+  //     // focusOnSelect: true
+  // });
 
-        autoplay: false,
-
-        speed: 2000,
-
-        dotsClass: 'pagination-dots',
-
-        // asNavFor: '.pagination-dots',
-
-    });
-
-    // $('.img-navigation').slick({ // настройка навигации
-    //     // slidesToShow: 3, // указываем что нужно показывать 3 навигационных изображения
-    //     asNavFor: '.slider', // указываем что это навигация для блока выше
-    //     focusOnSelect: true // указываем что бы слайделось по клику
-    // });
-
-    // $(".pagination-dots").slick({
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     asNavFor: ".slider",
-    //     // nextArrow: ".slider__next",
-    //     // prevArrow: ".slider__prev",
-    //     focusOnSelect: true
-    // });
-
-    function addClass(){
-        $('li').find('.img-navigation').removeClass('active');
-        $('.slick-active').find('.img-navigation').addClass('active');
-    }
-    addClass();
-
-    $('li').on("click", function () {
-        addClass();
-    });
-
-    $('.slide-navigation').on('click', function(){
-        addClass();
-    });
-
+  // $('.pagination-dots').slick({
+  //     // slidesToShow: 3,x
+  //     asNavFor: '.slider-for',
+  //     dots: false,
+  //     // centerMode: true,
+  //     focusOnSelect: true,
+  // });
+  //
+  // function addClass() {
+  //   $('li').find('.img-navigation').removeClass('active');
+  //   $('.slick-active').find('.img-navigation').addClass('active');
+  // }
+  // addClass();
+  //
+  // $('li').on('click', () => {
+  //   addClass();
+  // });
+  //
+  // $('.slide-navigation').on('click', () => {
+  //   addClass();
+  // });
 });
