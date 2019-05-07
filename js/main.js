1d67f10d3051d5c8cdb05198f743e3e353cf2a89
AlexMilenkiy <alex.milenkiy@gmail.com>
2019-05-07 11:48:24 +0300

    fixed work modules

--- js/main.js
+++ js/main.js
@@ -215,12 +215,17 @@
     "validate-js": 13
   }],
   5: [function (require, module, exports) {
-    require('./main-page/main-page.js');
+    (function (global) {
+      require('./main-page/main-page.js');
 
-    require('./slider/slider.js');
+      global.jQuery = require('jquery');
+
+      require('./slider/slider.js');
+    }).call(this, typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {});
   }, {
     "./main-page/main-page.js": 1,
-    "./slider/slider.js": 11
+    "./slider/slider.js": 11,
+    "jquery": 12
   }],
   6: [function (require, module, exports) {
     if (document.querySelector('.slider-headline')) {
@@ -878,102 +883,107 @@
     if (document.querySelector('.slider-slick-headline')) {
       require('../../slick/slick.js');
 
-      const slider = $('.slider');
-      const paging = $('.pagination-dots');
-      const images = Array.from($('.slider img'));
-      images.forEach((item, i) => {
-        const newDots = document.createElement('button');
-        newDots.classList.add('img-navigation');
-        newDots.innerText = i + 1;
-        document.querySelector('.pagination-dots').appendChild(newDots);
-      });
-      slider.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        dots: false,
-        arrows: true,
-        appendArrows: '.pagination-block',
-        prevArrow: '<button class="slide-navigation">Назад</button>',
-        nextArrow: '<button class="slide-navigation">Вперед</button>',
-        autoplay: false,
-        speed: 2000
-      });
-      paging.slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: false,
-        infinite: false,
-        focusOnSelect: false,
-        prevArrow: '<button class="lessImg"><<</button>',
-        nextArrow: '<button class="moreImg">>></button>'
-      });
-      const lessButton = $('.lessImg');
-      const moreButton = $('.moreImg');
-      let indexImg = 0;
-      paging.on('click', '.slick-slide', function (event) {
-        event.preventDefault();
-        const goToSingleSlide = $(this).data('slick-index');
-        slider.slick('slickGoTo', goToSingleSlide);
-        addHidden();
-      });
-      lessButton.on('click', () => {
-        addClass(indexImg);
-        addHidden();
-      });
-      moreButton.on('click', () => {
-        addClass(indexImg);
-        addHidden();
-      });
-      slider.on('afterChange', (event, slick, currentSlide) => {
-        paging.slick('slickCurrentSlide', currentSlide);
-        indexImg = currentSlide;
-        const currrentNavSlideElem = `.pagination-dots .slick-slide[data-slick-index="${currentSlide}"]`;
-        addClass(currentSlide);
-        $('.slick-slide').find('.img-navigation').removeClass('active');
-        $(currrentNavSlideElem).find('.img-navigation').addClass('active');
-        addHidden();
-      });
-      paging.find('.slick-slide').addClass('slider-nav');
-      paging.find('.slick-list').addClass('slick-list-nav');
-      paging.find('.slick-current').find('.img-navigation').addClass('active');
+      jQuery(() => {
+        const $ = jQuery;
+        const slider = $('.slider');
+        const paging = $('.pagination-dots');
+        const images = Array.from($('.slider img'));
+        images.forEach((item, i) => {
+          const newDots = document.createElement('button');
+          newDots.classList.add('slide-dots');
+          newDots.innerText = i + 1;
+          document.querySelector('.pagination-dots').appendChild(newDots);
+        });
+        slider.slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          adaptiveHeight: true,
+          dots: false,
+          arrows: true,
+          appendArrows: '.pagination-block',
+          prevArrow: '<button class="slide-navigation">Назад</button>',
+          nextArrow: '<button class="slide-navigation">Вперед</button>',
+          autoplay: false,
+          speed: 2000
+        });
+        paging.slick({
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: false,
+          infinite: false,
+          focusOnSelect: false,
+          prevArrow: '<button class="lessImg"><<</button>',
+          nextArrow: '<button class="moreImg">>></button>'
+        });
+        const lessButton = $('.lessImg');
+        const moreButton = $('.moreImg');
+        let indexImg = 0;
+        paging.on('click', '.slick-slide', function (event) {
+          event.preventDefault();
+          const goToSingleSlide = $(this).data('slick-index');
+          slider.slick('slickGoTo', goToSingleSlide);
+          addHidden();
+        });
+        lessButton.on('click', () => {
+          addClass(indexImg);
+          addHidden();
+        });
+        moreButton.on('click', () => {
+          addClass(indexImg);
+          addHidden();
+        });
+        slider.on('afterChange', (event, slick, currentSlide) => {
+          paging.slick('slickCurrentSlide', currentSlide);
+          indexImg = currentSlide;
+          const currrentNavSlideElem = `.pagination-dots .slick-slide[data-slick-index="${currentSlide}"]`;
+          addClass(currentSlide);
+          $('.slick-slide').find('.img-navigation').removeClass('active');
+          $(currrentNavSlideElem).find('.img-navigation').addClass('active');
+          addHidden();
+        });
+        paging.find('.slick-slide').addClass('slider-nav');
+        paging.find('.slick-list').addClass('slick-list-nav');
+        paging.find('.slick-current').find('.img-navigation').addClass('active');
 
-      function addClass(index) {
-        const currentNavPaging = Array.from(paging.find('.slick-active'));
-        lessButton.removeClass('active');
-        moreButton.removeClass('active');
+        function addClass(index) {
+          const currentNavPaging = Array.from(paging.find('.slick-active'));
+          lessButton.removeClass('active');
+          moreButton.removeClass('active');
 
-        if (currentNavPaging[0].getAttribute('data-slick-index') > index) {
-          lessButton.addClass('active');
+          if (currentNavPaging[0].getAttribute('data-slick-index') > index) {
+            lessButton.addClass('active');
+          }
+
+          if (currentNavPaging[2].getAttribute('data-slick-index') < index) {
+            moreButton.addClass('active');
+          }
         }
 
-        if (currentNavPaging[2].getAttribute('data-slick-index') < index) {
-          moreButton.addClass('active');
+        function addHidden() {
+          lessButton.removeClass('hidden');
+          moreButton.removeClass('hidden');
+
+          if (lessButton.attr('aria-disabled') === 'true') {
+            lessButton.addClass('hidden');
+          }
+
+          if (moreButton.attr('aria-disabled') === 'true') {
+            moreButton.addClass('hidden');
+          }
         }
-      }
 
-      function addHidden() {
-        lessButton.removeClass('hidden');
-        moreButton.removeClass('hidden');
-
-        if (lessButton.attr('aria-disabled') === 'true') {
-          lessButton.addClass('hidden');
-        }
-
-        if (moreButton.attr('aria-disabled') === 'true') {
-          moreButton.addClass('hidden');
-        }
-      }
-
-      addHidden();
-      slider.on('setPosition', () => {
-        slider.find('.slick-slide').height('auto');
-        const slickTrack = $('.body-block').find('.slider');
-        const slickTrackHeight = $(slickTrack).height();
-        slider.find('img').css('height', `${slickTrackHeight}px`);
-      });
-      paging.on('setPosition', () => {
-        paging.find('.slick-slide').width('50px');
+        addHidden();
+        slider.on('setPosition', () => {
+          slider.find('.slick-slide').height('auto');
+          const slickTrack = $('.body-block').find('.slider');
+          const slickTrackHeight = $(slickTrack).height();
+          slider.find('img').css('height', `${slickTrackHeight}px`);
+        });
+        paging.on('setPosition', () => {
+          paging.find('.slick-slide').css({
+            "min-width": "30px"
+          });
+        });
       });
     }
   }, {
